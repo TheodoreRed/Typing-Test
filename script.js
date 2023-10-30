5fd1e0d056ab2c0adfec728bf8c8de1eca35ba8d
Theodore Redlon <redlontheo@gmail.com>
2023-10-30 17:50:18 -0400

    Add reset functionality, expand testData, and enhance game flow

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
 
 const startBtn = document.getElementById("start-btn");
+const resetBtn = document.getElementById("reset-btn");
 const timer = document.getElementById("timer");
 const testParagraph = document.getElementById("test-paragraph");
 const userInput = document.getElementById("user-input");
 let timerInterval;
-let time = 59;
+let time = 60;
 let gameActive = false;
 const testData = [
   "The early bird catches the worm.",
@@ -18,6 +19,43 @@ const testData = [
   "Fortune favors the bold.",
   "People who live in glass houses should not throw stones.",
   "When the going gets tough, the tough get going.",
+  "In our village, folks say God crumbles up the old moon into stars.",
+  "One must be careful of books, and what is inside them, for words have the power to change us.",
+  "To be fond of dancing was a certain step towards falling in love.",
+  "Memory is the diary that we all carry about with us.",
+  "There are few people whom I really love, and still fewer of whom I think well.",
+  "Life appears to me too short to be spent in nursing animosity or registering wrongs.",
+  "I took a deep breath and listened to the old bray of my heart. I am. I am. I am.",
+  "There is no greater agony than bearing an untold story inside you.",
+  "Beware; for I am fearless, and therefore powerful.",
+  "You pierce my soul. I am half agony, half hope.",
+  "There is no charm equal to tenderness of heart.",
+  "It is a curious thought, but it is only when you see people looking ridiculous that you realize just how much you love them.",
+  "Have a heart that never hardens, and a temper that never tires, and a touch that never hurts.",
+  "The voice of the sea speaks to the soul.",
+  "I am not afraid of storms, for I am learning how to sail my ship.",
+  "Every atom of your flesh is as dear to me as my own: in pain and sickness it would still be dear.",
+  "With every day, and from both sides of my intelligence, the moral and the intellectual, I thus drew steadily nearer to the truth.",
+  "It is a truth universally acknowledged.",
+  "To sleep: perchance to dream.",
+  "So we beat on, boats against the current.",
+  "Heaven knows we need never be ashamed of our tears.",
+  "I could easily forgive his pride, if he had not mortified mine.",
+  "The world is full of obvious things.",
+  "Whatever our souls are made of, his and mine are the same.",
+  "Dreams, books, are each a world.",
+  "There is no friend as loyal as a book.",
+  "Some birds are not meant to be caged.",
+  "Every unhappy family is unhappy in its own way.",
+  "We live as we dream, alone.",
+  "It's no use going back to yesterday.",
+  "People disappear when they die.",
+  "Beauty is in the eye of the beholder.",
+  "All's well that ends well.",
+  "Fortune favors the brave.",
+  "The best way out is always through.",
+  "Time flies over us, but leaves its shadow behind.",
+  "To see a world in a grain of sand.",
 ];
 let mistake = 0;
 let correct = 0;
@@ -36,18 +74,32 @@ const shuffle = (array) => {
   }
 };
 
+const showResults = () => {};
+
 const reset = () => {
   clearInterval(timerInterval);
   gameActive = false;
-  calculateAccuracy(testParagraph.textContent, userInput.value, true);
-  console.log(mistake, correct);
+  startBtn.disabled = false;
+  testParagraph.style.display = "none";
+  userInput.disabled = true;
+  time = 60;
+  timer.textContent = time;
+  userInput.value = "";
+  mistake = 0;
+  correct = 0;
+  userInput.classList.remove("correct-input", "error-input");
+  indexNextSentence = 0;
+  console.clear();
 };
 
 const updateTimer = () => {
   timer.classList.toggle("animate-timer");
-  timer.textContent = time;
   time--;
+  timer.textContent = time;
   if (time < 0) {
+    calculateAccuracy(testParagraph.textContent, userInput.value, true);
+    console.log(mistake, correct);
+    showResults();
     reset();
   }
 };
@@ -83,7 +135,8 @@ const calculateAccuracy = (
     userSentenceArray.pop();
   }
 
-  let length = userSentenceArray.length;
+  let length = Math.min(testSentenceArray.length, userSentenceArray.length);
+
   for (let i = 0; i < length; i++) {
     if (testSentenceArray[i] !== userSentenceArray[i]) {
       mistake++;
@@ -100,8 +153,7 @@ const calculateAccuracy = (
 let indexNextSentence = 0;
 const moveToNextSentence = () => {
   if (indexNextSentence === testData.length) {
-    reset();
-    alert("All sentences completed!");
+    alert("All sentences completed!"); // This should never happen
   } else {
     calculateAccuracy(testParagraph.textContent, userInput.value);
     console.log(mistake, correct);
@@ -135,10 +187,17 @@ userInput.addEventListener("keydown", (e) => {
 });
 
 startBtn.addEventListener("click", () => {
+  shuffle(testData);
+  testParagraph.textContent = testData[indexNextSentence];
+  indexNextSentence++;
   timerInterval = setInterval(updateTimer, 1000);
   gameActive = true;
   startBtn.disabled = true;
   testParagraph.style.display = "inline";
   userInput.disabled = false;
   userInput.focus();
+});
+
+resetBtn.addEventListener("click", () => {
+  reset();
 });
